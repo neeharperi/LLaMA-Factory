0ca3ad638639f5fd7a3058458d1addfe9bd8fe71
Neehar Peri <neeharperi@users.noreply.github.com>
2026-01-14 16:18:37 -0500

    Implement mosaic and mixup augmentation methods
    Added apply_mosaic and apply_mixup functions for data augmentation. Updated the main function to utilize these augmentations during training.

--- rf20vl_sharegpt4v/data_augmentation.py
+++ rf20vl_sharegpt4v/data_augmentation.py
@@ -26,6 +26,125 @@ def get_albumentations_pipeline(width=640, height=640):
             p=0.5
         ),
     ], bbox_params=A.BboxParams(format='coco', label_fields=['category_ids'], min_visibility=0.1))
+    
+def apply_mosaic(image, bboxes, category_ids, buffer, output_size=(640, 640)):
+    """
+    Simulates CachedMosaic by combining the current image with 3 random images from the buffer.
+    """
+    if len(buffer) < 3:
+        return image, bboxes, category_ids
+
+    indices = random.sample(range(len(buffer)), 3)
+    mosaic_imgs = [image] + [buffer[i]['image'] for i in indices]
+    mosaic_anns = [{'bboxes': bboxes, 'category_ids': category_ids}] + \
+                  [{'bboxes': buffer[i]['bboxes'], 'category_ids': buffer[i]['category_ids']} for i in indices]
+
+    h, w = output_size[0], output_size[1]
+    c = 3
+    mosaic_img = np.full((h * 2, w * 2, c), 114, dtype=np.uint8) # YOLOX grey fill
+    
+    yc = int(random.uniform(0.5 * h, 1.5 * h))
+    xc = int(random.uniform(0.5 * w, 1.5 * w))
+
+    new_bboxes = []
+    new_categories = []
+
+    for i, (img_part, ann_part) in enumerate(zip(mosaic_imgs, mosaic_anns)):
+        oh, ow, _ = img_part.shape
+        
+        img_part = cv2.resize(img_part, (w, h))
+        h_part, w_part, _ = img_part.shape
+        
+        scale_x = w / ow
+        scale_y = h / oh
+        
+        if i == 0:  # Top-left
+            x1a, y1a, x2a, y2a = max(xc - w_part, 0), max(yc - h_part, 0), xc, yc
+            x1b, y1b, x2b, y2b = w_part - (x2a - x1a), h_part - (y2a - y1a), w_part, h_part
+        elif i == 1:  # Top-right
+            x1a, y1a, x2a, y2a = xc, max(yc - h_part, 0), min(xc + w_part, w * 2), yc
+            x1b, y1b, x2b, y2b = 0, h_part - (y2a - y1a), min(w_part, x2a - x1a), h_part
+        elif i == 2:  # Bottom-left
+            x1a, y1a, x2a, y2a = max(xc - w_part, 0), yc, xc, min(yc + h_part, h * 2)
+            x1b, y1b, x2b, y2b = w_part - (x2a - x1a), 0, w_part, min(h_part, y2a - y1a)
+        elif i == 3:  # Bottom-right
+            x1a, y1a, x2a, y2a = xc, yc, min(xc + w_part, w * 2), min(yc + h_part, h * 2)
+            x1b, y1b, x2b, y2b = 0, 0, min(w_part, x2a - x1a), min(h_part, y2a - y1a)
+
+        mosaic_img[y1a:y2a, x1a:x2a] = img_part[y1b:y2b, x1b:x2b]
+        pad_w = x1a - x1b
+        pad_h = y1a - y1b
+
+        current_bboxes = ann_part['bboxes']
+        current_cats = ann_part['category_ids']
+        
+        for bbox, cat in zip(current_bboxes, current_cats):
+            bx, by, bw, bh = bbox
+            
+            sbx = bx * scale_x
+            sby = by * scale_y
+            sbw = bw * scale_x
+            sbh = bh * scale_y
+            
+            nbx = sbx + pad_w
+            nby = sby + pad_h
+            nbw = sbw 
+            nbh = sbh 
+            
+            new_bboxes.append([nbx, nby, nbw, nbh])
+            new_categories.append(cat)
+
+    final_img = mosaic_img 
+    
+    valid_bboxes = []
+    valid_cats = []
+    
+    clip_h, clip_w = final_img.shape[:2]
+    
+    for bbox, cat in zip(new_bboxes, new_categories):
+        x, y, w_b, h_b = bbox
+        x1, y1 = max(0, x), max(0, y)
+        x2, y2 = min(clip_w, x + w_b), min(clip_h, y + h_b)
+        
+        if x2 > x1 and y2 > y1:
+            valid_bboxes.append([x1, y1, x2-x1, y2-y1])
+            valid_cats.append(cat)
+            
+    return final_img, valid_bboxes, valid_cats
+
+def apply_mixup(image, bboxes, category_ids, buffer, output_size=(640, 640)):    
+    idx = random.randint(0, len(buffer) - 1)
+    mix_img = buffer[idx]['image']
+    mix_bboxes = buffer[idx]['bboxes']
+    mix_cats = buffer[idx]['category_ids']
+    
+    # Resize both to target
+    img1 = cv2.resize(image, output_size)
+    img2 = cv2.resize(mix_img, output_size)
+    
+    # Blend
+    alpha = random.uniform(0.5, 0.8) # Mixup ratio
+    mixed_img = cv2.addWeighted(img1, alpha, img2, 1 - alpha, 0)
+    
+    final_bboxes = []
+    final_cats = []
+    
+    # Scale function
+    def scale_boxes(boxes, cats, orig_shape, target_shape):
+        sh, sw = orig_shape[:2]
+        th, tw = target_shape[:2]
+        res = []
+        for b in boxes:
+            res.append([b[0]*(tw/sw), b[1]*(th/sh), b[2]*(tw/sw), b[3]*(th/sh)])
+        return res
+        
+    final_bboxes.extend(scale_boxes(bboxes, category_ids, image.shape, output_size))
+    final_cats.extend(category_ids)
+    
+    final_bboxes.extend(scale_boxes(mix_bboxes, mix_cats, mix_img.shape, output_size))
+    final_cats.extend(mix_cats)
+    
+    return mixed_img, final_bboxes, final_cats
 
 def get_annotations(coco_json):
     res = {}
@@ -112,8 +231,10 @@ def main(args):
         }
         
         annotations_dict = get_annotations(train_json)
+
+        image_buffer = []
+        MAX_BUFFER_SIZE = 20
         
-        # ID Counters
         new_img_id = 0
         new_ann_id = 0
         
@@ -129,11 +250,25 @@ def main(args):
                 bboxes = [a['bbox'] for a in anns]
                 category_ids = [a['category_id'] for a in anns]
 
+                image_buffer.append({'image': image, 'bboxes': bboxes, 'category_ids': category_ids})
+                if len(image_buffer) > MAX_BUFFER_SIZE:
+                    image_buffer.pop(0)
+                    
                 final_image = image.copy()
                 final_bboxes = copy.deepcopy(bboxes)
                 final_cats = copy.deepcopy(category_ids)
                 
                 if epoch > no_aug_epochs:
+                    # We need to fix mosaic and mixup for few-shot detection
+                    #if random.random() < 0.5 and len(image_buffer) > 4:
+                    #    final_image, final_bboxes, final_cats = apply_mosaic(
+                    #        final_image, final_bboxes, final_cats, image_buffer, output_size=target_size
+                    #        )
+                    #elif random.random() < 0.5 and len(image_buffer) > 1:
+                    #    final_image, final_bboxes, final_cats = apply_mixup(
+                    #        final_image, final_bboxes, final_cats, image_buffer, output_size=target_size
+                    #        )
+                        
                     transformed = transform_pipeline(
                         image=final_image, 
                         bboxes=final_bboxes, 
